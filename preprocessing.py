b4ce7d09fd892480fba85b29f1f5729f9b22851c
George Ashmore <Georgeashmore@gmail.com>
2024-02-18 17:14:19 +0000

    ADD new hash to reference each window

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,16 @@
 from pyspark.sql.types import FloatType, LongType, StructType, StructField, StringType, IntegerType, ArrayType
 from pyspark.sql import SparkSession
 import os
-from pprint import pprint
 import time
 import cv2
 import numpy as np
 from skimage.feature import hog
+import hashlib
 
 spark = SparkSession.builder.getOrCreate()
 SOURCE_TABLE = "facial_recognition.image_path"
 TARGET_TABLE = "facial_recognition.historgram_oriented_gradients"
-MAX_DF_SIZE = 3000
+MAX_DF_SIZE = 2000
 
 def process_image(path):
     image = cv2.imread(path.ImagePath)
@@ -38,6 +38,7 @@ def process_image(path):
             y = int(slice_step[1] * col)
 
             image_slice = image[x:x+slice_shape[0], y:y+slice_shape[1]]
+            slice_hash = hashlib.md5(image_slice.tobytes()).hexdigest()
 
             cell_shape = (8, 8)
             cells_per_block = (2,2)
@@ -53,6 +54,7 @@ def process_image(path):
 
             hog_results.append(
                 {
+                    "hash": slice_hash,
                     "path":path.ImagePath,
                     "feature_descriptors": list(map(float, fd)),
                     "x": int(x),
@@ -74,18 +76,19 @@ def main():
             target = spark.read.table(TARGET_TABLE).select("path")
             df = df.join(target, df.ImagePath == target.path,"anti").limit(MAX_DF_SIZE)
         else:
-            df.limit(MAX_DF_SIZE)
+            df = df.limit(MAX_DF_SIZE)
         
         count = df.count()
+        print(count)
         if count == 0:
             break
-        print(count)
 
         df = df.repartition(df.count())
 
         df = df.rdd.flatMap(process_image)
 
         schema = StructType([
+                        StructField("hash", StringType(), False),
                         StructField("path",StringType(), False),
                         StructField("feature_descriptors", ArrayType(FloatType(), False), False),
                         StructField("x", IntegerType(), False),
@@ -99,8 +102,7 @@ def main():
         if target_table_exists:
             df.writeTo(TARGET_TABLE).using("iceberg").append()
         else:
-            df.writeTo(TARGET_TABLE).using("iceberg").getOrCreate()
-
+            df.writeTo(TARGET_TABLE).using("iceberg").createOrReplace()
 
 if __name__ == "__main__":
     main()
